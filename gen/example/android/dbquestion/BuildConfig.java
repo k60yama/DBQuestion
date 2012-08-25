81f2951c8f1f58a8f634e1097c0f216649ce3f8f
k60yama <k60yama@gmail.com>
2012-08-25 14:50:42 +0900

    retry_commit

---
+++ gen/example/android/dbquestion/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package example.android.dbquestion;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/example/android/dbquestion/DBQuestionActivity.java
@@ -0,0 +1,13 @@
+package example.android.dbquestion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DBQuestionActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apply_layout);
+    }
+}
